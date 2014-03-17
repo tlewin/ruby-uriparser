c5f7ce68816263e06727f96c5d6fc145971805a3
Thiago Lewin <thiago_lewin@yahoo.com.br>
2014-03-17 20:06:01 -0300

    benchmark for Addressable::URI parser

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -1,5 +1,6 @@
 require 'benchmark'
 require 'uri'
+require 'addressable/uri'
 
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 
@@ -8,16 +9,18 @@ require 'uriparser'
 
 n = ARGV[0] ? ARGV[0].to_i : 10_000
 
-Benchmark.bmbm(10) do |x|
+Benchmark.bmbm(12) do |x|
   puts 'Complex URLs'
   x.report('URI') {n.times {URI.parse('https://www.ruby-lang.org/en/?a=1#test')}}
   x.report('UriParser') {n.times {UriParser.parse('https://www.ruby-lang.org/en/?a=1#test')}}
+  x.report('Addressable') {n.times {Addressable::URI.parse('https://www.ruby-lang.org/en/?a=1#test')}}
 end
 
-puts '#' * 20
+puts "\n" << '#' * 20 << "\n"
 
-Benchmark.bmbm(10) do |x|
+Benchmark.bmbm(12) do |x|
   puts 'Simple URLs'
   x.report('URI') {n.times {URI('https://www.ruby-lang.org/en/')}}
   x.report('UriParser') {n.times {UriParser.parse('https://www.ruby-lang.org/en/')}}
+  x.report('Addressable') {n.times {Addressable::URI.parse('https://www.ruby-lang.org/en/')}}
 end
